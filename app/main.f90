e67909d14e640ba31427b81fe3791b5ee5172903
Diego Cali <diegocali123@gmail.com>
2024-02-14 18:15:34 -0600

    Added menu

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,33 @@
 program main
   use EDD_PROYECT_202201128, only: say_hello
   implicit none
-
+  logical          :: running
+  integer          :: option
+  running = .TRUE.
   call say_hello()
+  do while(running)
+    print *, "PLEASE SELECT AN OPTION:"
+    print *, "1. Initial parameters"
+    print *, "2. Execute Step"
+    print *, "3. Structures memory state"
+    print *, "4. Reports"
+    print *, "5. About me"
+    print *, "6. Exit"
+    read *, option
+    select case (option)
+      case (1)
+        print *, "Nothing yet"
+      case (2)
+        print *, "Nothing yet"
+      case (3)
+        print *, "Nothing yet"
+      case (4)
+        print *, "Nothing yet"
+      case (5)
+        print *, "Nothing yet"
+      case (6)
+        running = .FALSE.
+    end select
+  end do
+  print *, "//----------Program terminated----------\\"
 end program main
